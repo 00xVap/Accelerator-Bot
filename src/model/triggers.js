282a3dcf0be73d5b92650b805cd4b2dc252f3000
root <root@Accelerator-v1.0>
2023-07-04 13:19:24 +0000

    updated files

--- src/model/triggers.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-let triggerSchema = new Schema({
-  Guild: String,
-  Command: String,
-  Response: String,
-});
-
-module.exports = model("TriggerSchema", triggerSchema);
